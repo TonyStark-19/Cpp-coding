29651d02d59bb627c7c08f6417e8d1f5d7af072b
Aditya chandel <adityachandel371@gmail.com>
2024-12-13 17:46:27 +0530

    Sum and product of numbers in array

---
+++ Array data structure/SumProductInArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void sumArr(int arr[], int size);
+void productArr(int arr[], int size);
+
+int main()
+{
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = sizeof(arr) / sizeof(int);
+    int sum = 0;
+
+    sumArr(arr, size);
+    productArr(arr, size);
+
+    return 0;
+}
+
+void sumArr(int arr[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    cout << "Sum : " << sum << endl;
+}
+
+void productArr(int arr[], int size)
+{
+    int product = 1;
+    for (int i = 0; i < size; i++)
+    {
+        product *= arr[i];
+    }
+
+    cout << "Product : " << product << endl;
+}
