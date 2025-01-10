64a5f61ed879689a25d3599f57357e37388e7e81
Aditya chandel <adityachandel371@gmail.com>
2025-01-10 18:59:43 +0530

    Valid palindrome

---
+++ Cpp programs/ValidPalindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isAlphaNum(char ch)
+{
+    if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z'))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isPalindrome(string s)
+{
+    int st = 0, end = s.length() - 1;
+
+    while (st < end)
+    {
+        if (!isAlphaNum(s[st]))
+        {
+            st++;
+            continue;
+        }
+        if (!isAlphaNum(s[end]))
+        {
+            end--;
+            continue;
+        }
+        if (tolower(s[st]) != tolower(s[end]))
+        {
+            return false;
+        }
+
+        st++;
+        end--;
+    }
+    return true;
+}
+
+int main()
+{
+
+    string str = "Ac3?e3c&a";
+
+    cout << isPalindrome(str) << endl;
+
+    return 0;
+}
