bcc3be21569f0065b48007558ef1d589ba53da16
Aditya chandel <adityachandel371@gmail.com>
2025-02-01 20:48:37 +0530

    Quick sort algorithm

---
+++ Recursion/QuickSortAlgorithm.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &arr, int st, int end)
+{
+    int idx = st - 1, pivot = arr[end];
+
+    for (int j = st; j < end; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            idx++;
+            swap(arr[j], arr[idx]); // left partition
+        }
+    }
+
+    idx++;
+    swap(arr[end], arr[idx]); // assigning pivot the right index
+
+    return idx;
+}
+
+void quickSort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int pivIdx = partition(arr, st, end);
+
+        quickSort(arr, st, pivIdx - 1);  // left half call
+        quickSort(arr, pivIdx + 1, end); // right half call
+    }
+}
+
+int main()
+{
+    vector<int> arr = {12, 31, 35, 8, 32, 17};
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
