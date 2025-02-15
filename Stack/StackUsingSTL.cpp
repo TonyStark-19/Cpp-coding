8989a8dea0bd54e2838743e14b85994366903b20
Aditya chandel <adityachandel371@gmail.com>
2025-02-15 21:01:33 +0530

    Stack implementation using STL

---
+++ Stack/StackUsingSTL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+// Stack implementation using STL
+
+int main()
+{
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
