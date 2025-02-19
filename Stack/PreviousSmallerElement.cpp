da5414b044538e20a2005113496f63a621d2b409
Aditya chandel <adityachandel371@gmail.com>
2025-02-19 21:09:36 +0530

    Previous Smaller Element

---
+++ Stack/PreviousSmallerElement.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> prevSmallerElement(vector<int> arr)
+{
+    stack<int> s;
+    vector<int> ans(arr.size(), 0);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        while (s.size() > 0 && s.top() >= arr[i]) // removing invlaid values
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 1, 0, 8, 6};
+
+    vector<int> ans = prevSmallerElement(arr);
+
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
