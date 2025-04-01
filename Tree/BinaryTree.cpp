47e98eddb8184f96d7c34a7afee572e89740154d
Aditya chandel <adityachandel371@gmail.com>
2025-04-01 20:21:01 +0530

    Implemented traversals

--- Tree/BinaryTree.cpp
+++ Tree/BinaryTree.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
+// building BT and implementing different tree traversals
 
 // Node class
 class Node
@@ -40,15 +42,128 @@ Node *buildTree(vector<int> preorder)
     return root;
 }
 
+// preorder traversal
+void preOrder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // print root
+    cout << root->data << " ";
+
+    // recursive call for left sub tree
+    preOrder(root->left);
+
+    // recursive call for right sub tree
+    preOrder(root->right);
+}
+
+// inorder traversal
+void inOrder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // recursive call for left sub tree
+    inOrder(root->left);
+
+    // print root
+    cout << root->data << " ";
+
+    // recursive call for right sub tree
+    inOrder(root->right);
+}
+
+// postorder traversal
+void postOrder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // recursive call for left sub tree
+    postOrder(root->left);
+
+    // recursive call for right sub tree
+    postOrder(root->right);
+
+    // print root
+    cout << root->data << " ";
+}
+
+// level order traversal
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+
+    // push root
+    q.push(root);
+    // to mark level of tree
+    q.push(NULL);
+
+    while (q.size() > 0)
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        if (curr == NULL)
+        {
+            // next line case
+            if (!q.empty())
+            {
+                cout << endl;
+                q.push(NULL);
+                continue;
+            }
+            // empty tree case
+            else
+            {
+                break;
+            }
+        }
+
+        cout << curr->data << " ";
+
+        // push left sub tree
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+
+        // push right sub tree
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node *root = buildTree(preorder);
-    
-    cout << root->data << endl;
-    cout << root->left->data << endl;
-    cout << root->right->data << endl;
+
+    preOrder(root);
+    cout << endl;
+
+    inOrder(root);
+    cout << endl;
+
+    postOrder(root);
+    cout << endl;
+
+    levelOrder(root);
 
     return 0;
 }
